052a305e12f6f56356e45da5a685f3c35e711515
TheCoreMan <dude500@gmail.com>
2015-01-16 21:38:55 +0200

    Adding the game app and view
    Did manage.py startapp game. Added a basic view, and basic URL. Based on
    the django tutorial in
    https://docs.djangoproject.com/en/1.7/intro/tutorial03/

---
+++ web/prisoner/game/views.py
@@ -0,0 +1,7 @@
+from django.http import HttpResponse
+
+# Create your views here.
+
+
+def index(request):
+    return HttpResponse("This is just a temporary page, lol. I'll get around to it...")
--- web/prisoner/prisoner/urls.py
+++ web/prisoner/prisoner/urls.py
@@ -1,10 +1,8 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'prisoner.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
+urlpatterns = patterns(
+    '',
+    url(r'^game/', include('game.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
