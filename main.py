8dbcca0ab1c9849577fc05f274497b321cbd6515
Barak Nehmad <bnehmad@gmail.com>
2015-01-16 18:18:27 +0200

    Result function
    Added function to clean start_game up.

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 print("Start.")
 
 choices = ["Keep quiet", "Rat out"]
+countA = 0
+countB = 0
 
 def player_a_strategy():
     return 1
@@ -10,28 +12,38 @@ def player_a_strategy():
 def player_b_strategy():
     return 0
 
+def result(res):
+    global countA
+    global countB
+    if res == "A":
+        print("Player A won!")
+        countB += 3
+    if res == "B":
+        print("Player B won!")
+        countA += 3
+    if res == "AB":
+        print("Both players win :)")
+        countA += 1
+        countB += 1
+    if res == "0":
+        print("Both players lose :(")
+        countA += 2
+        countB += 2
+
 def start_game(player_a_strategy_arg, player_b_strategy_arg):
     player_a_choice = player_a_strategy_arg()
     player_b_choice = player_b_strategy_arg()
-    countA = 0
-    countB = 0
 
     if player_a_choice == 0:
         if player_b_choice == 0:
-            print("Both players win :)")
-            countA += 1
-            countB += 1
+            result("AB")
         else:
-            print("Player B won!")
-            countA += 3
+            result("B")
     else:
         if player_b_choice == 0:
-            print("Player A won!")
-            countB += 3
+            result("A")
         else:
-            print("Both players lose :(")
-            countA += 2
-            countB += 2
+            result("0")
     print("Number of years in prison for prisoner A: " + str(countA))
     print("Number of years in prison for prisoner B: " + str(countB))
 
