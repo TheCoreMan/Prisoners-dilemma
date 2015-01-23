254595cfcafa2f8fafe2ed27cdba081d9551e237
TheCoreMan <dude500@gmail.com>
2015-01-23 20:13:02 +0200

    Added results page
    Form submission works. The form's content is sent to the results page
    which only displays it for now.

--- web/prisoner/game/views.py
+++ web/prisoner/game/views.py
@@ -4,3 +4,8 @@ from django.shortcuts import render
 def index(request):
     context = {'text': "Welcome to the prisoner game."}
     return render(request, 'game/index.html', context)
+
+
+def results(request):
+    context = {'name': request.POST['name'], 'strategy_text': request.POST['strategy']}
+    return render(request, 'game/results.html', context)
--- web/prisoner/prisoner/urls.py
+++ web/prisoner/prisoner/urls.py
@@ -4,6 +4,6 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    url(r'^', include('game.urls')),
+    url(r'^', include('game.urls', namespace="game")),
     url(r'^admin/', include(admin.site.urls)),
 )
