6a6a38079c4a2f70428f95a490870e4d8887da63
TheCoreMan <dude500@gmail.com>
2015-02-01 20:39:34 +0200

    WIP new web pages
    dirty

--- web/prisoner/game/urls.py
+++ web/prisoner/game/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
+    url(r'^first_prisoner$', views.first_prisoner, name='first_prisoner'),
+    url(r'^second_prisoner$', views.second_prisoner, name='second_prisoner'),
     url(r'^results$', views.results, name='results'),
     url(r'^invalid_code$', views.results, name='invalid_code'),
 )
--- web/prisoner/game/views.py
+++ web/prisoner/game/views.py
@@ -10,17 +10,63 @@ def is_valid_python(code):
     return True
 
 
+class Prisoner(object):
+    def __init__(self, name, strategy):
+        self.name = name
+        self._strategy_text = strategy
+        assert is_valid_python(self._strategy_text), "Syntax error in strategy."
+
+    def __repr__(self):
+        return "Prisoner object. name: " + self.name + "\nstrategy: + " + self._strategy_text
+
+
+def save_first_prisoner(prisoner):
+    print("*** first prisoner \n" + repr(prisoner))
+
+
+def save_second_prisoner(prisoner):
+    print("*** second prisoner \n" + repr(prisoner))
+
+
+def process_first_prisoner(request):
+    try:
+        first_prisoner = Prisoner(name=request.POST['name'], strategy=request.POST['strategy'])
+        save_first_prisoner(first_prisoner)
+
+    except AssertionError as e:  # means bad strategy code
+        context = {'name': request.POST['name'], 'strategy_text': request.POST['strategy']}
+        return render(request, 'game/invalid_code.html', context)
+
+
+def process_second_prisoner(request):
+    try:
+        second_prisoner = Prisoner(name=request.POST['name'], strategy=request.POST['strategy'])
+        save_second_prisoner(second_prisoner)
+
+    except AssertionError as e:  # means bad strategy code
+        context = {'name': request.POST['name'], 'strategy_text': request.POST['strategy']}
+        return render(request, 'game/invalid_code.html', context)
+
+
 def index(request):
-    context = {'text': "Welcome to the prisoner game."}
+    context = {'title': "Welcome to the Prisoner's Dilemma..."}
     return render(request, 'game/index.html', context)
 
 
+def first_prisoner(request):
+    context = {'title': "Welcome to the Prisoner's Dilemma...", 'prisoner_number': "1", "dest_page": "second_prisoner"}
+    return render(request, 'game/prisoner.html', context)
+
+
+def second_prisoner(request):
+    process_first_prisoner(request)
+
+    context = {'title': "Welcome to the Prisoner's Dilemma...", 'prisoner_number': "2", "dest_page": "results"}
+    return render(request, 'game/prisoner.html', context)
+
+
 def results(request):
-    code = request.POST['strategy']
-    print("Is " + code + " valid?" + str(is_valid_python(code)))
-    if is_valid_python(code):
-        context = {'name': request.POST['name'], 'strategy_text': code}
-        return render(request, 'game/results.html', context)
-    else:
-        context = {'name': request.POST['name'], 'strategy_text': code}
-        return render(request, 'game/invalid_code.html', context)
+    process_second_prisoner(request)
+
+    context = {'name': "the prisoner names", 'strategy_text': "the prisoner strats, played out."}
+    return render(request, 'game/results.html', context)
